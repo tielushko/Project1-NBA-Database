241fcc8f657ad786dd97c894fe5f678429b9a5ef
Oleg Tielushko <tielushko@mail.usf.edu>
2021-01-16 15:56:21 -0500

    create coach class repr and modify constructor

--- coach.py
+++ coach.py
@@ -1,6 +1,7 @@
 class Coach:
-    def __init__(self, coach_id, first_name, last_name, season_win, season_loss, playoff_win, playoff_loss, team):
+    def __init__(self, coach_id, season, first_name, last_name, season_win, season_loss, playoff_win, playoff_loss, team):
         self.coach_id = coach_id
+        self.season = season
         self.first_name = first_name
         self.last_name = last_name
         self.season_win = season_win
@@ -9,6 +10,7 @@ class Coach:
         self.playoff_loss = playoff_loss
         self.team = team
 
-
+    def __repr__(self):
+        return "{0:<10} {1:<10} {2:<10} {3:<10} {4:<10} {5:<10} {6:<10} {7:<10} {8:<10}".format(self.coach_id,self.season, self.first_name, self.last_name, self.season_win, self.season_loss, self.playoff_win, self.playoff_loss, self.team)
 
 
